61067dd21b79818aed02e750050781bd29270117
Jon Janelle <jonjanelle1@gmail.com>
2017-06-16 13:58:01 -0400

    asdf

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module BostonJazzGigs
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.paths << Rails.root.join('vendor', 'assets','stylesheets')
-    config.assets.paths << Rails.root.join('vendor', 'assets','javascripts')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
