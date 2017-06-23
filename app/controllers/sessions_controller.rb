0e7806beb8ba41e8becf41bcef3d6088d6b31843
Jon Janelle <jonjanelle1@gmail.com>
2017-06-23 14:50:41 -0400

    Added footer and flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       redirect_to user
     end
-
+    flash[:danger]='Invalid email/password combination'
     render 'new'
   end
 
